b7c2e78f3ecc27bd86dce68b0150556e0c66e107
asterisktotle <barandajander@gmail.com>
2025-07-31 18:56:04 +0800

    initialize database and env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,23 +3,41 @@ dotenv.configDotenv()
 
 import express from 'express'
 import cors from 'cors'
+import { initializeDB } from './config/db.ts';
+
 
 //Environments variable
 
 const app = express();
 const PORT = process.env.PORT || 3000;
  
+// Initialize data base
+
 
 // Middleware
 app.use(cors())
 app.use(express.json())
 
 // Routes
-app.get('/', (req,res) => {
-    res.send('Hello app')
-})
 
 
-app.listen(PORT, () => {
-    console.log('Server is running at http://localhost:'+ PORT)
-})
+
+
+// Start server with database
+const startServer = async () => {
+    try{
+        await initializeDB()
+
+        app.listen(PORT, () => {
+            console.log('Server is running at http://localhost:'+ PORT)
+        })
+
+
+    }catch(error){
+        console.error('Failed to start server: ', error)
+        process.exit(1)
+    }
+}
+
+
+startServer()
